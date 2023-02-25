13171562395b828ede6acc1a87deda7227cdfac1
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-02-25 22:51:56 +0300

    completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>
+    Directors Page
+    </h1>
+   
+      {
+        directors.map((dir,index)=>{
+          return  <div key={index}>
+                <h2>{dir.name}</h2>
+                <ul>{
+                  dir.movies.map((movie,index) => {
+                    return <li key={index}>{movie}</li>
+                  })
+                }</ul>
+           </div>
+        })
+        
+      }
+   
+  </div>;
 }
 
 export default Directors;
